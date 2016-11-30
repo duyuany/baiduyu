726fb76eabc81200ec3d95a2a538bfe1c258d81c
duyuany <1013672828@qq.com>
2016-11-30 23:50:34 +0800

    11.30

--- js/index.js
+++ js/index.js
@@ -37,13 +37,51 @@ $(function(){
     })
     $("#fullpage")[0].addEventListener("webkitTransitionEnd",function(){
         flag=true;
+        $("section").each(function(index,obj){
+            if(num==0){
+                return;
+            }
+            //if(num!=0){
+                if(index==num){
+                    $(".aa").eq(index).css({
+                        transform: "translateX(0px)",opacity: 1
+                    })
+                    $(".bb").eq(index).css({
+                        transform: "translateX(0px)",opacity: 1
+                    })
+
+                }else{
+                    $(".aa").eq(index).css({
+                        transform: "translateX(-50px)",opacity: 0
+                    })
+                    $(".bb").eq(index).css({
+                        transform: "translateX(50px)",opacity: 0
+                    })
+                }
+            //}
+
+
+        })
     })
     $("#fullpage")[0].addEventListener("mozTransitionEnd",function(){
         flag=true;
+        $(".con-wrap").each(function(index,obj){
+            $(obj).eq(index).css({
+                transform: "translateX(0px)",opacity: 1
+            })
+        })
+        $(".tree").each(function(index,obj){
+            $(obj).eq(index).css({
+                transform: "translateX(0px)",opacity: 1
+            })
+        })
     })
+
+
 
     var flag2=true;
     $(".menu-option").click(function(){
+        $(".menu").css({display:"block"});
         if(flag2){
             $(this).find(".menu-option-tline").css({transform: "rotateZ(45deg) translateY(10px)"})
             $(this).find(".menu-option-bline").css({transform: "rotateZ(-45deg) translateY(-10px)"})
@@ -65,6 +103,25 @@ $(function(){
                     animation: "menu1 2s ease "+(1.2-index*0.2)+"s forwards"})
             })
             flag2=true;
+            setTimeout(function(){
+                $(".menu").css({display:"none"});
+            },6000)
         }
     })
+    /*浏览器尺寸变化*/
+    $(window).resize(function(){
+        ch=$(window).height();
+        var cw=$(window).width();
+        $("#fullpage").css("margin-top",-ch*num);
+        if(cw>1000){
+            $(".menu a").css({
+                animation:"none",
+                opacity:0
+            })
+            $(".menu-option-tline").css({transform: "rotateZ(0deg) translateY(0px)"})
+            $(".menu-option-bline").css({transform: "rotateZ(0deg) translateY(0px)"})
+            flag2=true
+        }
+    })
+
 })
